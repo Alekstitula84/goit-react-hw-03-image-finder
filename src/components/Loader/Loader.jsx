e7df3b13124142527d5e436078f649efbfdde55c
Tytula Oleksandr <alekstitula84@gmail.com>
2023-04-07 17:12:48 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,17 +1,15 @@
-import { ColorRing } from 'react-loader-spinner';
+import { RotatingLines } from 'react-loader-spinner';
 import { LoaderWrapper } from './Loader.styled';
 
 const Loader = () => {
     return (
         <LoaderWrapper>
-            <ColorRing
+            <RotatingLines
+                strokeColor="tomato"
+                strokeWidth="5"
+                animationDuration="0.75"
+                width="96"
                 visible={true}
-                height="80"
-                width="80"
-                ariaLabel="blocks-loading"
-                wrapperStyle={{}}
-                wrapperClass="blocks-wrapper"
-                colors={['#b8c480', '#B2A3B5', '#F4442E', '#51E5FF', '#429EA6']}
             />
         </LoaderWrapper>
     );
